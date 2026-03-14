149e155b70804c6ec53d17fa6c261aa300568e3b
Anton <antontv2019@gmail.com>
2026-03-14 20:45:44 +0200

    added database to controller router

--- app/routers/controller_router.py
+++ app/routers/controller_router.py
@@ -5,18 +5,36 @@ from aiogram.filters import Command
 
 from app.utils.keyboards import create_main_keyboard
 
+from aiogram import Router, types
+from sqlalchemy.ext.asyncio import AsyncSession
+from sqlalchemy import select
+from app.db.models import User
+
 router = Router()
 load_dotenv()
 
 
 @router.message(Command("start"))
-async def cmd_start(message: types.Message):
+async def cmd_start(message: types.Message, session: AsyncSession):
 
+    result = await session.execute(select(User).where(User.telegram_id == message.from_user.id))
+    is_user = result.scalar_one_or_none()
     keyboard = create_main_keyboard()
     text = ("👋Привіт!\nЯ бот для запису на мийку vianor. \n"
-            "Обирай дію, я допоможу тобі тут з усім")
+            "Обирай дію, я допоможу тобі тут з усім\n")
 
-    await message.reply(
-        text= text,
-        reply_markup= keyboard.as_markup()
-    )
+    if not is_user:
+        new_user = User(telegram_id=message.from_user.id, full_name=message.from_user.full_name)
+        session.add(new_user)
+        await session.commit()
+
+        text += "Новий юзер"
+        await message.answer(
+            text= text,
+            reply_markup= keyboard.as_markup()
+        )
+    else:
+        await message.reply(
+            text= text,
+            reply_markup= keyboard.as_markup()
+        )
